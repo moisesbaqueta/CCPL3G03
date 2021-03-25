c48c03be540659cf26213cb3b5720ebe3b4bd676
Kiprin <79595270+Kiprin@users.noreply.github.com>
2021-03-25 16:48:16 +0000

    Update parser.c

--- code/parser/parser.c
+++ code/parser/parser.c
@@ -54,3 +54,60 @@ void parse (char *line){
             long Y = POP (s);
             long X = POP (s);
             PUSH(s, X / Y);
+        /**
+        * exponencialização
+        */
+        }else if (strcmp(token, "#")== 0 ){
+            long Y = POP (s);
+            long X = POP (s);
+            PUSH(s, pow(X,Y));
+        /**
+        * incrementa uma unidade 
+        */
+        }else if (strcmp(token, ")")== 0 ){
+            long Y = POP (s);
+            PUSH(s, ++Y);
+        /**
+        * decrementa uma unidade
+        */
+        }else if (strcmp(token, "(")== 0 ){
+            long Y = POP (s);
+            PUSH(s, --Y);
+        /**
+        * resto de uma divisao inteira
+        */
+        }else if (strcmp(token, "%")== 0 ){
+            long Y = POP (s);
+            long X = POP (s);
+            PUSH(s, X % Y);
+        /**
+        * bits em comum
+        */
+        }else if (strcmp(token, "&")== 0 ){
+            long Y = POP (s);
+            long X = POP (s);
+            PUSH(s, Y & X );
+        /**
+        * coloca a zero todos os bits em comum e a 1 os bits diferentes 
+        */
+        }else if (strcmp(token, "^")== 0 ){
+            long Y = POP (s);
+            long X = POP (s);
+            PUSH(s, Y ^ X);
+        /**
+        * troca os bits de um nr binario
+        */
+        }else if (strcmp(token, "~")== 0 ){
+            long Y = POP (s);
+            PUSH(s,(~Y));
+        /**
+        *  uniao de bits entre dois nr binarios 
+        */
+        }else if (strcmp(token, "|")== 0 ){
+            long Y = POP (s);
+            long X = POP (s);
+            PUSH(s, Y | X ); 
+        }   
+    }
+    print_stack(s); 
+}
