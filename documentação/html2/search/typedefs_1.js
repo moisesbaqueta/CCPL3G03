57dcda97856791c9f66e1c0eede0910b3e1b382e
Francisca95776 <79987453+Francisca95776@users.noreply.github.com>
2021-04-25 22:38:47 +0100

    Add files via upload

---
+++ documentação/html2/search/typedefs_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['stack_98',['STACK',['../stack2_8h.html#ade8bb1f37e07eb511f3873082665d739',1,'stack2.h']]]
+];
---
+++ documentação/html2/search/variables_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['double_90',['DOUBLE',['../structdata.html#aecc3f36c8ad5b0d05fee3f3372aee7d8',1,'data']]]
+];
