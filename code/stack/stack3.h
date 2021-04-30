5d9cf7871fbcd2d8bf3d3b6acb55f64d89dfb457
Srpedrofelix <79987248+Srpedrofelix@users.noreply.github.com>
2021-04-30 16:18:21 +0100

    Update stack3.h

--- code/stack/stack3.h
+++ code/stack/stack3.h
@@ -19,3 +19,31 @@ typedef struct data {
 /**
  *  \brief struct que define um array com 26 elementos que representam as letras do abecedário e estrutura da stack , com uma variavel que dá o tamanho e o numero de elementos 
  */
+typedef struct stack {
+  DATA array[26];
+  DATA *stack;
+  int size;
+  int n_elems;
+} STACK;
+
+int has_type(DATA elem, int mask);
+STACK *create_stack();
+
+void push(STACK *s, DATA elem);
+DATA pop(STACK *s);
+DATA top(STACK *s);
+DATA enesimo(STACK *s, int n);
+void print_stack(STACK *s);
+/**
+ *  Macro macro que faz a substituiçao de acordo com o stack operation correspondente para podermos ter uma forma rapida de um push e pop para cada tipo 
+ */
+#define STACK_OPERATION_PROTO(_type, _name)   \
+  void push_##_name(STACK *s, _type val);     \
+  _type pop_##_name(STACK *s);
+
+STACK_OPERATION_PROTO(long, LONG)
+STACK_OPERATION_PROTO(double, DOUBLE)
+STACK_OPERATION_PROTO(char, CHAR)
+STACK_OPERATION_PROTO(char *, STRING)
+
+#endif
