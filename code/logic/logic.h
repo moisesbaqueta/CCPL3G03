68add6e1142035864eef207b544b2e7ab7b3c9a6
Moisés <64909792+moisesbaqueta@users.noreply.github.com>
2021-04-30 16:08:07 +0100

    Add files via upload

---
+++ code/logic/logic.h
@@ -0,0 +1,17 @@
+void EMAIOR(STACK *s);
+
+void EMENOR(STACK *s);
+
+void BIGGER(STACK *s);
+
+void SMALLER(STACK *s);
+
+void EQUAL(STACK *s);
+
+void NAO(STACK *s);
+
+void eShortcout(STACK *s);
+
+void ouShortcout(STACK *s);
+
+void ifThenElse(STACK *s);
