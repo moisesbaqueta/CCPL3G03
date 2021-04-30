f818a6aea7aa9b564b2ccef46331057087a6d4e7
Francisca95776 <79987453+Francisca95776@users.noreply.github.com>
2021-04-30 16:21:24 +0100

    Update logic.c

--- code/logic/logic.c
+++ code/logic/logic.c
@@ -126,3 +126,128 @@ if(has_type(x, LONG) && has_type(y, LONG)) {
         r.LONG = 0 ;  
     }else{ 
         r.LONG = 1; }
+   }else if (has_type(x, LONG) && has_type(y, CHAR)) {  
+    if(x.LONG > y.CHAR){
+        r.LONG = 0 ;  
+    }else{ 
+        r.LONG = 1; }
+}else if (has_type(x, CHAR) && has_type(y, DOUBLE)) {   
+    if(x.CHAR > y.DOUBLE){
+        r.LONG = 0 ;  
+    }else{ 
+        r.LONG = 1; }
+} else if (has_type(x, DOUBLE) && has_type(y, CHAR)) {    
+    if(x.DOUBLE > y.CHAR){
+        r.LONG = 0 ;  
+    }else{ 
+        r.LONG = 1; }
+
+} else if (has_type(x, CHAR) && has_type(y, CHAR)) {      
+   if(x.CHAR > y.CHAR){
+        r.LONG = 0 ;  
+    }else{ 
+        r.LONG = 1; }
+    } 
+ push(s,r); 
+}
+/**
+ * \brief funçao que coloca no topo da stack 0 (falso) se o topo for inferior ao penultimo elemento e 1 (verdadeiro) se for superior 
+ * @param s - passagem de stack como parametro  
+ */
+void SMALLER(STACK *s) {                                      
+    DATA x = pop(s);                                     
+    DATA y = pop(s);
+    DATA r;  
+    r.type = LONG;                                                                              
+if(has_type(x, LONG) && has_type(y, LONG)) {            
+    if(x.LONG < y.LONG){
+        r.LONG = 0 ;  
+    }else{ 
+        r.LONG = 1; }    
+} else if (has_type(x, DOUBLE) && has_type(y, LONG)) {   
+    if(x.DOUBLE < y.LONG){
+        r.LONG = 0 ;  
+    }else{ 
+        r.LONG = 1; } 
+} else if (has_type(x, DOUBLE) && has_type(y, DOUBLE)) { 
+    if(x.DOUBLE < y.DOUBLE){
+        r.LONG = 0 ;  
+    }else{ 
+        r.LONG = 1; } 
+} else if (has_type(x, LONG) && has_type(y, DOUBLE)) {   
+    if(x.LONG < y.DOUBLE){
+        r.LONG = 0 ;  
+    }else{ 
+        r.LONG = 1; } 
+} else if (has_type(x, CHAR) && has_type(y, LONG)) { 
+    if(x.CHAR < y.LONG){
+        r.LONG = 0 ;  
+    }else{ 
+        r.LONG = 1; } 
+} else if (has_type(x, LONG) && has_type(y, CHAR)) {  
+    if(x.LONG < y.CHAR){
+        r.LONG = 0 ;  
+    }else{ 
+        r.LONG = 1; }  
+} else if (has_type(x, CHAR) && has_type(y, DOUBLE)) {   
+    if(x.CHAR < y.DOUBLE){
+        r.LONG = 0 ;  
+    }else{ 
+        r.LONG = 1; } 
+} else if (has_type(x, DOUBLE) && has_type(y, CHAR)) {    
+    if(x.DOUBLE < y.CHAR){
+        r.LONG = 0 ;  
+    }else{ 
+        r.LONG = 1; } 
+}else if (has_type(x, CHAR) && has_type(y, CHAR)) {      
+    if(x.CHAR < y.CHAR){
+        r.LONG = 0 ;  
+    }else{ 
+        r.LONG = 1; }
+    } 
+ push(s,r); 
+}
+/**
+ * \brief funçao que coloca no topo da stack 1 (verdadeiro) se o topo e o penultimo forem iguais e 0 (falso) se forem diferentes 
+ * @param s - passagem de stack como parametro  
+ */
+void EQUAL(STACK *s) {                                      
+    DATA x = pop(s);                                     
+    DATA y = pop(s);
+    DATA r;
+    r.type = LONG;                                                                                
+if(has_type(x, LONG) && has_type(y, LONG)) {            
+    if(x.LONG == y.LONG){
+        r.LONG = 1 ;  
+    }else{ 
+        r.LONG = 0; }    
+} else if (has_type(x, DOUBLE) && has_type(y, LONG)) {   
+    if(x.DOUBLE == y.LONG){
+        r.LONG = 1 ;  
+    }else{ 
+        r.LONG = 0; }
+}else if (has_type(x, DOUBLE) && has_type(y, DOUBLE)) { 
+    if(x.DOUBLE == y.DOUBLE){
+        r.LONG = 1 ;  
+    }else{ 
+        r.LONG = 0; }
+} else if (has_type(x, LONG) && has_type(y, DOUBLE)) {   
+    if(x.LONG == y.DOUBLE){
+        r.LONG = 1 ;  
+    }else{ 
+        r.LONG = 0; }
+}else if (has_type(x, CHAR) && has_type(y, LONG)) { 
+    if(x.CHAR == y.LONG){
+        r.LONG = 1 ;  
+    }else{ 
+        r.LONG = 0; }
+} else if (has_type(x, LONG) && has_type(y, CHAR)) {  
+    if(x.LONG == y.CHAR){
+        r.LONG = 1 ;  
+    }else{ 
+        r.LONG = 0; }  
+} else if (has_type(x, CHAR) && has_type(y, DOUBLE)) {   
+    if(x.CHAR == y.DOUBLE){
+        r.LONG = 1 ;  
+    }else{ 
+        r.LONG = 0; }
